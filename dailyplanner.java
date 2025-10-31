ec4d374891e1816467212dd21ab3b632dd99f31d
trackhawkboys <boakyed0405@students.bowiestate.edu>
2025-10-31 15:25:59 -0400

    directory comments

---
+++ dailyplanner.java
@@ -0,0 +1,14 @@
+public class dailyplanner {
+public static  void main (String []args) { 
+
+// program should ask user for username and password 
+
+
+
+
+// prompt user to give type in tasks that need to be fufilled throughout the day 
+
+// store information and list the tasks for that specific including, day of the week, month, 
+
+} }
+
